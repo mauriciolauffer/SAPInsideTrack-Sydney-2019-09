a8ad1ab401f2daef0764c0c984cd61446b84ec3d
Mauricio Lauffer <443888+mauriciolauffer@users.noreply.github.com>
2019-08-25 10:49:04 +1000

    SAP Inside Track - Sydney

---
+++ webapp/Component.js
@@ -0,0 +1,17 @@
+sap.ui.define(["sap/ui/core/UIComponent", "sap/ui/core/ComponentSupport"], function(UIComponent) {
+  "use strict";
+
+  return UIComponent.extend("mlauffer.sapinsidetrack.sydney.201909.Component", {
+    metadata: {
+      manifest: "json"
+    },
+
+    init: function() {
+      UIComponent.prototype.init.apply(this, arguments);
+    },
+
+    destroy: function() {
+      UIComponent.prototype.destroy.apply(this, arguments);
+    }
+  });
+});
---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"], function(Controller) {
+  "use strict";
+
+  return Controller.extend("mlauffer.sapinsidetrack.sydney.201909.controller.App", {});
+});
